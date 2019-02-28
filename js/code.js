b679ab9044764c72aacbc81d2905431849887af2
hortasha <eidemartin_303@hotmail.com>
2019-02-28 12:55:23 +0100

    change slide on click

--- js/code.js
+++ js/code.js
@@ -1,33 +1,69 @@
 $(document).ready(() => {
-    $('#slider').css('width', '100%');
-    $('#slider').css('text-align', 'center');
-    $('#slider img').css('display', 'none');
-    $('#slider img').css('width', '75%');
+    const $slider = $('#slider');
+    const $imges = $('#slider img');
+    const $firstImg = $('#slider img:first');
 
-    $('#slider img:first').css('display', 'inline');
+    $slider.css('width', '100%');
+    $slider.css('text-align', 'center');
+    $imges.css('display', 'none');
+    $imges.css('width', '100%');
+    $imges.css('position', 'relative');
+    $imges.css('right', '1000px')
+    $firstImg.css('display', 'inline');
+    $firstImg.css('right', '0');
 
-    let slides = $('#slider img').length
-    let slide = 1;
-    let currentSlide = $('#slider img:first');
+
+    const slides = $imges.length - 1;
+    let slide = 0;
+
+    let slideNavItems = "";
+    $imges.each(function() {
+        slideNavItems += '<div class="slideNav--item"></div>';
+    });
+
+
+    $slider.append(
+        '<div id="slideNav">' +
+            slideNavItems +
+        '</div>'
+    );
+
+    const $slideItem = $('.slideNav--item');
+    
+    $slideItem.css('border-radius', '50%');
+    $slideItem.css('height', '10px');
+    $slideItem.css('width', '10px');
+    $slideItem.css('background-color', 'black');
+    $slideItem.css('display', 'inline-block');
+    $slideItem.css('margin', '5px');
+    $('.slideNav--item:first').css('background-color', 'red')
+    
+
+    $slideItem.click(function() {
+        nextSlide($slideItem.index(this), slide);
+        slide = $slideItem.index(this);
+    });
 
     setInterval(() => {
         if(slide < slides) {
-            nextSlide(currentSlide.next(), currentSlide);
-            currentSlide = currentSlide.next();
+            nextSlide(slide+1, slide);
             slide++;
         } else {
-            nextSlide($('#slider img:first'), currentSlide);
-            currentSlide = $('#slider img:first');
-            slide = 1;
+            nextSlide(0, slide);
+            slide = 0;
         }
     }, 5000);
 });
 
-function nextSlide(next, current) {
-    current.css('display', 'none');
-    next.css('width', '0');
-    next.css('display', 'inline');
-    next.animate({
-        width: '75%'
-    }, "fast");
+function nextSlide($next, $current) {
+    console.log($current, $next)
+    $($('#slider img')[$current]).css('display', 'none' );
+    $($('#slider img')[$current]).css('right', '1000px' );
+
+    $($('#slider img')[$next]).css('display', 'inline')
+    $($('#slider img')[$next]).animate({
+        right: '0'
+    }, 'fast', 'linear');
+    $($('.slideNav--item')[$current]).css('background-color', 'black')
+    $($('.slideNav--item')[$next]).css('background-color', 'red');
 }
